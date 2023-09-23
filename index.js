a8fb4a3eb58ed5167ed7e3f57a9bd5696ab80acd
william lee <will.lee.npc@gmail.com>
2023-09-23 05:01:23 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "unknown";
+upperCaseCustomerName = () => {customerName = customerName.toUpperCase()};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+};
+
+changeLeastFavoriteCustomer = () => {leastFavoriteCustomer = "changed"};
